945563af1bec0c064941ef7cf0269d20d0ba5a8e
임리안 <z1aaannsss@gmail.com>
2023-12-12 03:51:27 +0900

    오류 보안

--- app/src/main/java/network/myapplication/Chatting.java
+++ app/src/main/java/network/myapplication/Chatting.java
@@ -3,27 +3,35 @@ package network.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Chatting extends AppCompatActivity {
 
+    private Button btnLibrarianChat, btnStudyGroupChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatting);
 
-    }
+        btnLibrarianChat = findViewById(R.id.btnLibrarianChat);
+        btnStudyGroupChat = findViewById(R.id.btnStudyGroupChat);
 
+        btnLibrarianChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Chatting.this, LibrarianChatActivity.class);
+                startActivity(intent);
+            }
+        });
 
-    public void onLibrarianChatClick(View view) {
-        Intent intent = new Intent(this, LibrarianChatActivity.class);
-        startActivity(intent);
-    }
-
-
-    public void onStudyGroupChatClick(View view) {
-        Intent intent = new Intent(this, StudyGroupChatActivity.class);
-        startActivity(intent);
+        btnStudyGroupChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Chatting.this, StudyGroupChatActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
