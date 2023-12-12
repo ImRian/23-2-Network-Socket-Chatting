9fda00939f541167b7cd57aea0f322c1af589878
danidana2 <150885774+danidana2@users.noreply.github.com>
2023-12-12 23:00:37 +0900

    activity_chatting.xml, Chatting.java 수정
    ㅇㅇ

--- app/src/main/java/network/myapplication/Chatting.java
+++ app/src/main/java/network/myapplication/Chatting.java
@@ -4,19 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Chatting extends AppCompatActivity {
 
-    private Button btnLibrarianChat, btnStudyGroupChat;
+    private Button btnLibrarianChat, btnStudyGroupChat, btnStudyGroup, btnStudyRoom, btnChatting;
+    private ImageButton btnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatting);
 
-        btnLibrarianChat = findViewById(R.id.btnLibrarianChat);
-        btnStudyGroupChat = findViewById(R.id.btnStudyGroupChat);
+        btnLibrarianChat = findViewById(R.id.libChatBtn);
+        btnStudyGroupChat = findViewById(R.id.networkChatBtn);
+        btnStudyGroup = findViewById(R.id.chat2studyGroupBTN1);
+        btnStudyRoom = findViewById(R.id.chat2studyRoomBTN1);
+        btnChatting = findViewById(R.id.chat2chatBTN1);
+        btnHome = findViewById(R.id.chatHomeBTN1);
 
         btnLibrarianChat.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +37,37 @@ public class Chatting extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Chatting.this, StudyGroupChatActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnStudyGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Chatting.this, StudyCategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnStudyRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnChatting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Chatting.this, Chatting.class);
+                startActivity(intent);
+            }
+        });
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Chatting.this, MainActivity.class);
                 startActivity(intent);
             }
         });
