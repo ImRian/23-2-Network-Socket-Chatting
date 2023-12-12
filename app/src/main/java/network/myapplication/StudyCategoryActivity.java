8fbf667e181f4245a3fa0c47a85dce9ab1b26ffd
danidana2 <150885774+danidana2@users.noreply.github.com>
2023-12-12 16:39:28 +0900

    DAYOUNG
    DA

--- app/src/main/java/network/myapplication/StudyCategoryActivity.java
+++ app/src/main/java/network/myapplication/StudyCategoryActivity.java
@@ -2,14 +2,77 @@ package network.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class StudyCategoryActivity extends AppCompatActivity {
+
+    private Button btnJavaStudy, btnJsStudy, btnCStudy, btnNetworkStudy, btnStudyGroup, btnStudyRoom, btnChatting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_category);
+
+        btnJavaStudy = findViewById(R.id.javaStudyBtn);
+        btnJsStudy = findViewById(R.id.jsStudyBtn);
+        btnCStudy = findViewById(R.id.cStudyBtn);
+        btnNetworkStudy = findViewById(R.id.networkStudyBtn);
+        btnStudyGroup = findViewById(R.id.studyGroupBTN);
+        btnStudyRoom = findViewById(R.id.studyRoomBTN);
+        btnChatting = findViewById(R.id.chattingBTN);
+
+        btnJavaStudy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnJsStudy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnCStudy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnNetworkStudy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnStudyGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StudyCategoryActivity.this, StudyCategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnStudyRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnChatting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StudyCategoryActivity.this, Chatting.class);
+                startActivity(intent);
+            }
+        });
     }
 }
