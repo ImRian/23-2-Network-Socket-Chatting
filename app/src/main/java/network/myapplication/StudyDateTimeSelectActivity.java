1b60d79c6d31678cbd63342845d60255ed0e17a9
danidana2 <150885774+danidana2@users.noreply.github.com>
2023-12-12 21:43:44 +0900

    스터디 세부설정 화면 activity틀
    ㅇㅇ

---
+++ app/src/main/java/network/myapplication/StudyDateTimeSelectActivity.java
@@ -0,0 +1,14 @@
+package network.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudyDateTimeSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_date_time_select);
+    }
+}
