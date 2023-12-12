0f8dfbf32247f37bfecf9e61d053e8a06768b81f
danidana2 <150885774+danidana2@users.noreply.github.com>
2023-12-12 16:21:17 +0900

    dayoung
    dada

--- app/src/main/java/network/myapplication/MainActivity.java
+++ app/src/main/java/network/myapplication/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(MainActivity.this, StudyGroup.class);
+                Intent intent = new Intent(MainActivity.this, StudyCategoryActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/network/myapplication/StudyCategoryActivity.java
@@ -0,0 +1,15 @@
+package network.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class StudyCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_category);
+    }
+}
